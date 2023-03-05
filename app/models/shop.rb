4afa8fb227168aaca890b6d0f938ea22e9c9744e
Sushil <sushil10018@gmail.com>
2023-03-05 12:52:22 +0700

    refactor delegating category name to category through shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,6 +2,7 @@ class Shop < ApplicationRecord
   validates :name, :latitude, :longitude, presence: true
 
   belongs_to :category
+  delegate :name, to: :category, prefix: true
 
   def self.markers_hash
     Shop.includes(:category).all.map do |shop|
@@ -9,12 +10,12 @@ class Shop < ApplicationRecord
         lat: shop.latitude,
         lng: shop.longitude,
         shop_name: shop.name,
-        category_name: shop.category.name
+        category_name: shop.category_name
       }
     end
   end
 
   def icon_path
-    ActionController::Base.helpers.asset_path("#{category.name.downcase.parameterize}.png")
+    ActionController::Base.helpers.asset_path("#{category_name.downcase.parameterize}.png")
   end
 end
