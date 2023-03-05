6f9f171ff9e1efce518b9682e9a090beb79db962
Sushil <sushil10018@gmail.com>
2023-03-05 12:36:23 +0700

    prepare the markers hash required for all shops

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,15 @@ class Shop < ApplicationRecord
   validates :name, :latitude, :longitude, presence: true
 
   belongs_to :category
+
+  def self.markers_hash
+    Shop.includes(:category).all.map do |shop|
+      {
+        lat: shop.latitude,
+        lng: shop.longitude,
+        shop_name: shop.name,
+        category_name: shop.category.name
+      }
+    end
+  end
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe Shop, type: :model do
     expect(shop.errors[:longitude]).to include("can't be blank")
     expect(shop.errors[:category]).to include("must exist")
   end
+
+  it 'returns the markers hash of all shops' do
+    nails = Category.create(name: 'Nails')
+    Shop.create(name: 'Nail Shop', latitude: 13.5007, longitude: 100.3634, category: nails)
+    Shop.create(name: 'Saam Song Shop', latitude: 13.7307, longitude: 100.4534, category: nails)
+
+    hair_removal = Category.create(name: 'Hair removal')
+    Shop.create(name: 'Hair Removal Shop', latitude: 13.2307, longitude: 100.1234, category: hair_removal)
+
+    expect(Shop.markers_hash).to eq [
+      {lat: 13.5007, lng: 100.3634, shop_name: 'Nail Shop', category_name: 'Nails'},
+      {lat: 13.7307, lng: 100.4534, shop_name: 'Saam Song Shop', category_name: 'Nails'},
+      {lat: 13.2307, lng: 100.1234, shop_name: 'Hair Removal Shop', category_name: 'Hair removal'}
+    ]
+  end
 end
