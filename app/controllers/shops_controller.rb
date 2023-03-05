3873d0c800e18f21b593214351d72f201f15178d
Sushil <sushil10018@gmail.com>
2023-03-05 14:33:35 +0700

    implement rails caching to query the shops which expires daily

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,7 @@
 class ShopsController < ApplicationController
   def index
-    @markers = Shop.markers_hash
+    @markers = Rails.cache.fetch('markers_hash', expires_in: 1.day) do
+      Shop.markers_hash
+    end
   end
 end
